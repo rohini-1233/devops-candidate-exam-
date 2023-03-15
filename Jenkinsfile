6e8e69d293b0f32117177778c423a89f84cde4e1
unknown <Test@PNI6W11165.net.plm.eds.com>
2023-03-15 13:20:57 +0530

    jenkins file lambda updated

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,24 +1,33 @@
 pipeline{
     agent any
     stages{
+        stage("Install Terraform"){
+            steps{
+
+            }
+        }
         stage("TF Init"){
             steps{
                 echo "Executing Terraform Init"
+                sh (‘terraform init’)
             }
         }
         stage("TF Validate"){
             steps{
                 echo "Validating Terraform Code"
+                sh (‘terraform validate’)
             }
         }
         stage("TF Plan"){
             steps{
                 echo "Executing Terraform Plan"
+                sh (‘terraform plan’)
             }
         }
         stage("TF Apply"){
             steps{
                 echo "Executing Terraform Apply"
+                sh (‘terraform apply’)
             }
         }
         stage("Invoke Lambda"){
--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# S3 Backend bucket
 resource "aws_s3_bucket" "b" {
   bucket = "testtohinibuck"
 
@@ -9,12 +10,12 @@ resource "aws_s3_bucket" "b" {
 
 # Private Subnet
 resource "aws_subnet" "pvtsub" {
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = data.aws_vpc.vpc.id
   cidr_block = "10.0.1.0/24"
 }
 # raouting table
 resource "aws_route_table" "prvtrtable" {
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = data.aws_vpc.vpc.id
   tags = {
     Name = "tester 6"
   }
@@ -30,7 +31,7 @@ resource "aws_route_table" "prvtrtable" {
 resource "aws_route" "private_nat_gateway" {
   route_table_id         = aws_route_table.prvtrtable.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat.id
+  nat_gateway_id         = data.aws_nat_gateway.nat.id
 }
 
 resource "aws_route_table_association" "private" {
@@ -40,3 +41,32 @@ resource "aws_route_table_association" "private" {
 }
 
 # lambda function
+provider "archive" {}
+data "archive_file" "zip" {
+  type        = "zip"
+  source_file = "welcome.py"
+  output_path = "welcome.zip"
+}
+data "aws_iam_policy_document" "policy" {
+  statement {
+    sid    = ""
+    effect = "Allow"
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type        = "Service"
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.policy.json
+}
+resource "aws_lambda_function" "lambda" {
+  function_name = "welcome"
+  filename         = data.archive_file.zip.output_path
+  source_code_hash = data.archive_file.zip.output_base64sha256
+  role    = data.aws_iam_role.lambda.name
+  handler = "welcome.lambda_handler"
+  runtime = "python3.6"
+}
